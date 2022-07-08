1cd4cdf7a3d49c795583ee374d6d8a4ed15ef8f9
Ibrohim2001 <ikhayitmirzaev@gmail.com>
2022-07-08 21:43:18 +0500

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BsSearch } from "react-icons/bs";
+import axios from "axios";
 
-function App() {
+import "./App.css";
+import Today from "./components/today/Today";
+import Hourly from "./components/hour/Hourly";
+
+function App(props) {
+  // const KEY = "1690ecd304e4cd392179dcf6dd2a3a99";
+  const KEY = "e7091047cd8746f2b92164257221906";
+
+  const [weatherData, setWeatherData] = useState({});
+  const [location, setLocation] = useState("");
+  // const current = new Date()
+  // const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`
+  const [today, setToday] = useState(true);
+  const [hours, setHours] = useState(false);
+  const [days, setDays] = useState(false);
+
+  const URL = `https://api.weatherapi.com/v1/forecast.json?key=${KEY}&q=${location}&days=3&aqi=no&alerts=no`;
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    axios.get(URL)
+          .then(response => {
+            setWeatherData(response.data);
+            console.log(response.data)
+          })
+          .catch(err => console.log(err));
+          setLocation('');
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <form className="search">
+        <h2>MyWeather App</h2>
+        <div className="search_group">
+          <input
+            value={location}
+            onChange={event => setLocation(event.target.value)}
+            // onKeyPress={searchLocation}
+            placeholder='Enter Location'
+            type="text" 
+          />
+          <button onClick={handleSearch} type='submit' className="search_btn">
+            <BsSearch className="search_icon" />
+          </button>
+        </div>
+      </form>
+      <div className="container">
+        <div className="top_content">
+          <div className="main_info">
+            <div className="main_top">
+              <h1 className="temp">{weatherData?.current?.temp_c}<span>°C</span></h1>
+              <div>
+                <img src={weatherData?.current?.condition?.icon} alt="" />
+                <p>{weatherData?.current?.condition?.text}</p>
+              </div>
+            </div>
+            <div className="location_name">
+              <h2>{weatherData?.location?.name}</h2>
+              <p>{weatherData?.location?.country}</p>
+            </div>
+          </div>
+          <div className="time_container">
+            <h3>{weatherData?.location?.localtime?.slice(10, 16)}</h3>
+            <p>{weatherData?.location?.localtime?.slice(0, 10)}</p>
+          </div>
+        </div>
+        <div className="bottom_content">
+          <ul className="forecast_menu">
+            <li><button className="forecast_btn">Today's forecast</button></li>
+            <li><button className="forecast_btn">Hourly forecast</button></li>
+            <li><button className="forecast_btn">Daily forecast</button></li>
+          </ul>
+          <div className="extra_info">
+            <Today weatherData={weatherData
+            }/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/today/Today.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import './Today.css';
+import { FiSunrise, FiSunset } from 'react-icons/fi'
+
+
+
+const Today = ({weatherData}) => {
+
+  return (
+    <div className='todays_info'>
+      <div className="left">
+        <h4>Feels like <span>{weatherData?.current?.feelslike_c} °C</span></h4>
+        <h4>Humidity <span>{weatherData?.current?.humidity} %</span></h4>
+        <h4>Pressure <span>{weatherData?.current?.pressure_mb} mb</span></h4>
+        <h4>Wind <span>{weatherData?.current?.wind_kph} kph</span></h4>
+      </div>
+      <div className="right">
+        <div className="card">
+          <FiSunrise className='sun_icon'/>
+          <h3>Sunrise</h3>
+          <p>{weatherData?.forecast?.forecastday[0].astro.sunrise}</p>
+        </div>
+        <div className="card">
+          <FiSunset className='sun_icon'/>
+          <h3>Sunset</h3>
+          <p>{weatherData?.forecast?.forecastday[0].astro.sunset}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Today
